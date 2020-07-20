0b3c949ba09b7fec955f13e2d5667b46f3f58da7
hupeng <bankhp@163.com>
2020-07-20 09:09:37 +0800

    add name query tool

--- sfcx.py
+++ sfcx.py
@@ -6,6 +6,7 @@ def sfcx(account, begin_date, end_date=''):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname='10.0.134.110', port=22, username='hisusr', password='hisusr')
+    # ssh.connect(hostname='192.168.57.128', port=22, username='db2inst1', password='db2inst1')
 
     stdin, stdout, stderr = ssh.exec_command(
         '. /dbhome/hisusr/.profile;sh /datatmp/sjm/YX_SFCX/cx.sh {account} {begin_date} {end_date}'
@@ -16,7 +17,9 @@ def open_date_query(id_no='', org_no='', account='', name=''):
     # 开启SSH通道
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname='10.0.134.110', port=22, username='hisusr', password='hisusr')
+    # ssh.connect(hostname='10.0.134.110', port=22, username='hisusr', password='hisusr')
+    ssh.connect(hostname='192.168.57.128', port=22, username='db2inst1', password='db2inst1')
+
 
     if id_no:
         stdin, stdout, stderr = ssh.exec_command(
@@ -32,8 +35,10 @@ def open_date_query(id_no='', org_no='', account='', name=''):
                 .format(account=account))
     if name:
         stdin, stdout, stderr = ssh.exec_command(
-            '. /dbhome/hisusr/.profile;sh /datatmp/sjm/YX_SFCX/kh_mc_dg.sh {name}'
-                .format(name=name))
+            'echo {name} > {name}.txt'.format(name=name))
+        # stdin, stdout, stderr = ssh.exec_command(
+        #     '. /dbhome/hisusr/.profile;sh /datatmp/sjm/YX_SFCX/kh_mc_dg.sh {name}'
+        #         .format(name=name))
 
 
 def pos_query(account, date, amt):
